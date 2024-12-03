a92443a096be8438b5a4c4018dad0dd3d9eba07d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-12-03 14:08:19 +0300

    final

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,17 @@
-import {Outlet,Navigate,Route,Routes,useLocations} from 'react-router-dom'
+import { Outlet, Navigate, Route, Routes, useLocations } from 'react-router-dom'
 
-import {Footer,Navbar} from "../components/Navbar"
+import { Footer, Navbar } from "../components/Navbar"
 
 export default function App() {
-  return (
-    <main>
-        <Navbar/>
-
-        <Footer/>
-    </main>
-  )
+    return (
+        <main>
+            <Navbar />
+            <Routes>
+                <Routes>
+                    <Route element={<Layout />}></Route>
+                </Routes>
+            </Routes>
+            <Footer />
+        </main>
+    )
 }
